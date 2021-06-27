f89081159a80c7aaddb24feb072e3422fe7fc39c
Matúš Babinský <matusbabinsky@yahoo.com>
2021-06-27 20:20:16 +0200

    rest - getById response 404 update

--- src/main/java/com/example/bam/controllers/PersonController.java
+++ src/main/java/com/example/bam/controllers/PersonController.java
@@ -4,6 +4,8 @@ import com.example.bam.controllers.interfaces.IPersonGetter;
 import com.example.bam.controllers.services.PersonGetter;
 import com.example.bam.types.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,9 +29,14 @@ public class PersonController {
         return personGetter.getAllPeople();
     }
 
-    @RequestMapping(value = "/all/{personId}", method = RequestMethod.GET)
-    public Person getPersonById(@PathVariable long personId) {
-        return personGetter.getPersonById(personId);
+    @RequestMapping(value = "/get/{personId}", method = RequestMethod.GET)
+    public ResponseEntity<Person> getPersonById(@PathVariable long personId) {
+        return getPersonByIdResponse(personId);
+    }
+
+    private ResponseEntity<Person> getPersonByIdResponse(long personId) {
+        Person p = personGetter.getPersonById(personId);
+        return p != null ? new ResponseEntity<>(p, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
